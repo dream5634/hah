aaf3597317bc19972dd5cb13b67960116e79dc69
dream5634 <53044966+dream5634@users.noreply.github.com>
2020-07-30 15:01:51 +0800

    Create index.js
    new

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("Hello World"); 
+function myFunction(a, b) {
+  return a + b;             
+}
+var x = myFunction(5, 10); 
+
+console.log(x);
+
+console.log("I caused a merge conflict.");
